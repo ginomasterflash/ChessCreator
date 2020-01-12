0651aa8d31fed0c9eb62a1d2120e1648d0df30e1
Mattia Renna <mattia.renna@live.com>
2020-01-12 20:28:39 +0100

    initial commit

---
+++ ChessCreator.Core/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessCreator.Core
+{
+    /// <summary>
+    /// The application state as a view model
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The current page of the application
+        /// </summary>
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+
+        /// <summary>
+        /// True if the side menu should be shown
+        /// </summary>
+        public bool SideMenuVisible { get; set; } = false;
+    }
+}
---
+++ ChessCreator/Dashboard.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Windows;
+
+namespace ChessCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            DataContext = new WindowViewModel(this);
+        }
+    }
+}
